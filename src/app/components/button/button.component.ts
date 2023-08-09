44c36788e909e59022e81fdbd96c9c2bde83c1a7
Newton Ombese <newtonombese1@gmail.com>
2023-08-09 17:42:00 +0300

    :art: Working on the header

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text!: string;
+  @Input() color!: string;
+  @Output() btnClick = new EventEmitter();
+
+  onClick() {
+    this.btnClick.emit();
+  }
+}
